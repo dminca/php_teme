8f31f9eacb46d40565124f7ac50445c35dc822e9
Mincă Daniel Andrei <andrei_17_09@hotmail.com>
2013-09-15 15:02:31 +0300

    Update c02_ex8.php

--- c02_ex8.php
+++ c02_ex8.php
@@ -1,20 +1,20 @@
 <html>
 <head>
-	<title></title>
+	<title>c02 ex8</title>
 </head>
 <body>
 
 <form method="get" action="">
 <label>Introduceti luna</label>
-<br>
-<input type="text" name="luna" value=""/>
-<br>
+<br/>
+<input type="text" name="Luna" value=""/>
+<br/>
 <input type="submit" name="" value="Afla"/>
 </form>
 
 <?php
 
-$luna = $_GET['luna'];
+$luna = $_GET['Luna'];
 
 /* UN MOD DE-A LUCRA ***
 switch ($luna) {
@@ -33,13 +33,23 @@ switch ($luna) {
 
 */
 
+switch ($luna) {
+
 case ($luna >= 6 && $luna <= 8):
 	echo "vara";
 	break;
 
-case ($luna >=9 && $luna <= 11)	:
+case ($luna >=9 && $luna <= 11):
 	echo "toamna";
 	break;
+
+case ($luna == 12 || ($luna>=1 && $luna <= 2)):
+	echo "iarna";
+	break;
+
+case ($luna >=3 && $luna <= 5):
+	echo "primavara";
+	break;	
 
 default:
 	echo "Valoare eronata";
