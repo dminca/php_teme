a46a6dcca86f22aa51a6ad91ad8678ba6944588e
Mincă Daniel Andrei <andrei_17_09@hotmail.com>
2013-09-22 16:52:26 +0300

    Refreshed again

---
+++ c03/classroom/destin.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "lista";
+
+//start select
+echo "<select> name=$name>";
+
+//generez el. option
+for ($i; $i<=31; $i++){
+	echo "<option val=\" $i \"> $i </option>";
+} 
+//end select
+echo  "</select>";
+
+
+
+
+?>
